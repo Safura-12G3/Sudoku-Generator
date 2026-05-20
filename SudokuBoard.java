7e7687a3bf84b36aac0f46cbc135396176bd91ea
Safura-12G3 <safurasaif9g3@gmail.com>
2026-05-20 17:45:59 +0400

    Implemented Sudoku rules validation
    Added core rule checking logic for Sudoku, ensuring that moves are validated in a specified row, column, and  3x3 grid.

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -17,4 +17,55 @@ ublic class SudokuBoard {
     public int getValue(int row, int col) {
         return board[row][col];
     }
+
+    // Core Rule Check
+    // This method checks if a number can be placed in a specific cell.
+    public boolean isValidMove(int row, int col, int number) {
+
+        return !usedInRow(row, number)
+                && !usedInCol(col, number)
+                && !usedInBox(row, col, number);
+    }
+
+
+    // Row Check
+    // This method checks if a number is already present in the specified row.
+    private boolean usedInRow(int row, int number) {
+        for (int col = 0; col < 9; col++) {
+            if (board[row][col] == number) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    // Column Check
+    // This method checks if a number is already present in the specified column.
+    private boolean usedInCol(int col, int number) {
+        for (int row = 0; row < 9; row++) {
+            if (board[row][col] == number) {
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    // 3x3 Box Check
+    // This method checks if a number is already present within the specified 3x3 grid.
+    private boolean usedInBox(int row, int col, int number) {
+
+        int boxRowStart = row - row % 3;
+        int boxColStart = col - col % 3;
+
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                if (board[boxRowStart + r] [boxColStart + c] == number) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
