02422109533d4e6a4419f64e032c360cf479e643
Safura-12G3 <safurasaif9g3@gmail.com>
2026-05-20 17:12:29 +0400

    Initial SudokuBoard Setup

---
+++ SudokuBoard.java
@@ -0,0 +1,20 @@
+ublic class SudokuBoard {
+
+    private int[][] board;
+
+    public SudokuBoard() {
+        board = new int[9][9];
+    }
+
+    public int[][] getBoard() {
+        return board;
+    }
+
+    public void setValue(int row, int col, int value) {
+        board[row][col] = value;
+    }
+
+    public int getValue(int row, int col) {
+        return board[row][col];
+    }
+}
